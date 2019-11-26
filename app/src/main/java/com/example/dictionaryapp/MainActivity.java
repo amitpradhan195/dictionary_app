dc9f9b8537ac952d788c5d74f349fa0dd34b2753
Amit Pradhan <amitpradhan195@gmail.com>
2019-11-26 08:01:29 +0545

    before Sqlite

--- app/src/main/java/com/example/dictionaryapp/MainActivity.java
+++ app/src/main/java/com/example/dictionaryapp/MainActivity.java
@@ -9,6 +9,10 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,55 +22,55 @@ public class MainActivity extends AppCompatActivity {
     private Map<String, String> dictionary;
 
     //array with key and value
-    public static final String words[] = {
-            "अब | aba","now, from now on",
-            "अबेला | abela","bound, tied up confined",
-            "अबेर | aber","late",
-            "अबेर भयो | aber bhayo","it is late",
-            "आभास | abhas","glimpse, shadow,\n" +"reflection, vision, faint\n" +"memory, inkling",
-            "अभाव | abhav","non-existence, absence,\n" +"lack, scarcity",
-            "अभय | abhaya","",
-            "अभिभावक | abhibhavak","guardian",
-            "अभिलेख | abhilekh","inscription",
-            "अभिनय | abhinaya","mimicry, imitation",
-            "अभिप्रेरित | abhiprerit","motivated",
-            "अभिरुचि | abhiruci","inclination, earnest desire,\n" +"liking",
-            "अभियोग | abhiyog","accusation, allegation,\n" +"charge",
-            "अभियुक्त | abhiyukta","accused",
-            "अभ्यास | abhyas","practice",
-            "अचम्म | acamma","surprise, wonder,\n" +"amazement",
-            "अचम्म लग्नु | acamma lagnu","to be surprised, to be\n" +"amazed, to be astonished",
-            "अचम्म पर्नु | acamma parnu","to be surprised, to be\n" +"amazed, to be astonished",
-            "अचानक | acanak","suddenly, abruptly,\n" +"unexpectedly",
-            "अचानो | acano","suddenly, abruptly,\n" +"unexpectedly",
-            "अचार | acar","hot and sour pickle, pickles,\n" +"chutney, salsa",
-            "आचरण | acaran","conduct, behaviour,\n" +"practice",
-            "अचेल | acel","these days, nowadays,\n" +"recently, presently",
-            "आड | aad","support, prop",
-            "अदालत | adalat","law cour",
-            "आदन - प्रदान | adan-pradan","giving and taking of things,\n" +"exchange, social\n" +"communication, give and\n" +"take",
-            "आदरणीय | adarniya","respectable, deserving\n" +"honour",
-            "आदरणीय सदस्य | adarniya sadasya","honorary member",
-            "आदर्श | adarsh","ideal, value",
-            "अदाउनु | adaunu","to support, to hold up, to\n" +"stop",
-            "अड्डा | aada","office; court of law",
-            "आदेश | adesh","order, instruction,\n" +"command",
-            "आधा | adha","half",
-            "अढाई बजे | adhai baje","half past two",
-            "आधार | adhar","foundation, basis, support",
-            "आधारात | adharat","midnight",
-            "आधारभूत | adharbhut","basic, fundamental",
-            "आधारित | adharit","based upon",
-            "अधर्म | adharma","sinfulness, irreligiousness",
-            "आधि | adhi","almost a half",
-            "अधिकार | adhikar","right, authority, procession,\n" +"proprietorship",
-            "अधिकारी | adhikari","officer, one having\n" +"authority, magistrate,\n" +"proprietor",
-            "अधिकृत | adhikrit","authorised, vested with\n" +"authority",
-            "अधिराज | adhiraj","supreme ruler, sovereign,\n" +"overlord",
-            "अधिराज्य | adhirajya","sovereignty, empire,\n" +"kingdom",
-            "अधिवेशन | adhiveshan","session, meeting, sitting",
-
-    };
+//    public static final String words[] = {
+//            "अब | aba","now, from now on",
+//            "अबेला | abela","bound, tied up confined",
+//            "अबेर | aber","late",
+//            "अबेर भयो | aber bhayo","it is late",
+//            "आभास | abhas","glimpse, shadow,\n" +"reflection, vision, faint\n" +"memory, inkling",
+//            "अभाव | abhav","non-existence, absence,\n" +"lack, scarcity",
+//            "अभय | abhaya","",
+//            "अभिभावक | abhibhavak","guardian",
+//            "अभिलेख | abhilekh","inscription",
+//            "अभिनय | abhinaya","mimicry, imitation",
+//            "अभिप्रेरित | abhiprerit","motivated",
+//            "अभिरुचि | abhiruci","inclination, earnest desire,\n" +"liking",
+//            "अभियोग | abhiyog","accusation, allegation,\n" +"charge",
+//            "अभियुक्त | abhiyukta","accused",
+//            "अभ्यास | abhyas","practice",
+//            "अचम्म | acamma","surprise, wonder,\n" +"amazement",
+//            "अचम्म लग्नु | acamma lagnu","to be surprised, to be\n" +"amazed, to be astonished",
+//            "अचम्म पर्नु | acamma parnu","to be surprised, to be\n" +"amazed, to be astonished",
+//            "अचानक | acanak","suddenly, abruptly,\n" +"unexpectedly",
+//            "अचानो | acano","suddenly, abruptly,\n" +"unexpectedly",
+//            "अचार | acar","hot and sour pickle, pickles,\n" +"chutney, salsa",
+//            "आचरण | acaran","conduct, behaviour,\n" +"practice",
+//            "अचेल | acel","these days, nowadays,\n" +"recently, presently",
+//            "आड | aad","support, prop",
+//            "अदालत | adalat","law cour",
+//            "आदन - प्रदान | adan-pradan","giving and taking of things,\n" +"exchange, social\n" +"communication, give and\n" +"take",
+//            "आदरणीय | adarniya","respectable, deserving\n" +"honour",
+//            "आदरणीय सदस्य | adarniya sadasya","honorary member",
+//            "आदर्श | adarsh","ideal, value",
+//            "अदाउनु | adaunu","to support, to hold up, to\n" +"stop",
+//            "अड्डा | aada","office; court of law",
+//            "आदेश | adesh","order, instruction,\n" +"command",
+//            "आधा | adha","half",
+//            "अढाई बजे | adhai baje","half past two",
+//            "आधार | adhar","foundation, basis, support",
+//            "आधारात | adharat","midnight",
+//            "आधारभूत | adharbhut","basic, fundamental",
+//            "आधारित | adharit","based upon",
+//            "अधर्म | adharma","sinfulness, irreligiousness",
+//            "आधि | adhi","almost a half",
+//            "अधिकार | adhikar","right, authority, procession,\n" +"proprietorship",
+//            "अधिकारी | adhikari","officer, one having\n" +"authority, magistrate,\n" +"proprietor",
+//            "अधिकृत | adhikrit","authorised, vested with\n" +"authority",
+//            "अधिराज | adhiraj","supreme ruler, sovereign,\n" +"overlord",
+//            "अधिराज्य | adhirajya","sovereignty, empire,\n" +"kingdom",
+//            "अधिवेशन | adhiveshan","session, meeting, sitting",
+//
+//    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -75,10 +79,11 @@ public class MainActivity extends AppCompatActivity {
         lstDictionary = findViewById(R.id.lstDictionary);
         dictionary = new HashMap<>();
 
-        for(int i =0; i<words.length;i+=2){
-            dictionary.put(words[i],words[i+1]);
-        }
+//        for(int i =0; i<words.length;i+=2){
+//            dictionary.put(words[i],words[i+1]);
+//        }
 
+        readFromFile();
         ArrayAdapter adapter = new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
@@ -91,14 +96,31 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long l) {
                 String key = parent.getItemAtPosition(position).toString();
+                String word = parent.getItemAtPosition(position).toString();
 
                 String meaning = dictionary.get(key);
                 System.out.println(meaning);
 
                 Intent intent = new Intent(MainActivity.this, MeaningActivity.class);
                 intent.putExtra("meaning",meaning);
+                intent.putExtra("word", word);
                 startActivity(intent);
             }
         });
+    }
+
+    private void readFromFile(){
+        try {
+            FileInputStream fos = openFileInput("Word.txt");
+            InputStreamReader isr = new InputStreamReader(fos);
+            BufferedReader br = new BufferedReader(isr);
+            String line="";
+            while ((line=br.readLine()) != null){
+                String[] parts = line.split("->");
+                dictionary.put(parts[0], parts[1]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/dictionaryapp/MeaningActivity.java
+++ app/src/main/java/com/example/dictionaryapp/MeaningActivity.java
@@ -1,28 +1,74 @@
 package com.example.dictionaryapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Locale;
+
 public class MeaningActivity extends AppCompatActivity {
 
-    private TextView tvmeaning;
+    private TextView tvMeaning;
+    private TextToSpeech convertToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meaning);
+        tvMeaning = findViewById(R.id.tvMeaning);
 
         Bundle bundle = getIntent().getExtras();
 
         if (bundle!=null){
             String meaning = bundle.getString("meaning");
-            tvmeaning = findViewById(R.id.meaning);
-            tvmeaning.setText(meaning);
+            String word = bundle.getString("word");
+
+            this.setTitle(word);
+            tvMeaning.setText(meaning);
+
         }else{
             Toast.makeText(this,"No Meaning",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.speak:
+                Bundle bundle = getIntent().getExtras();
+
+                if (bundle != null) {
+                    final String word = bundle.getString("meaning");
+                    convertToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+                        @Override
+                        public void onInit(int status) {
+                            if(status != TextToSpeech.ERROR){
+                                convertToSpeech.setLanguage(Locale.US);
+                                convertToSpeech.speak(word, TextToSpeech.QUEUE_FLUSH, null, null);
+                            }
+                        }
+                    });
+                }
+                return true;
+             default:
+                 return super.onOptionsItemSelected(item);
         }
     }
 }
